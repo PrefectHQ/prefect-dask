9578caf3a598dccc678fb4301a64f684922d1aec
ahuang11 <andrew.h@prefect.io>
2022-09-29 15:13:14 -0700

    Update exception docstring

--- prefect_dask/exceptions.py
+++ prefect_dask/exceptions.py
@@ -5,5 +5,5 @@ Exceptions specific to prefect-dask.
 
 class ImproperClientError(Exception):
     """
-    Raised when the flow/task is async but the client is sync, or vice versa.
+    Raised when the flow/task is async but the client is sync.
     """
